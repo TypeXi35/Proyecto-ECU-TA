ed7e99dbc507ef88a0319fd24c18e112830d02f5
Hector Romero <hector.romero@sopristec.com>
2026-08-06 21:01:58 -0600

    Created the main switrch and State Class

--- hitov1/ECU.cpp
+++ hitov1/ECU.cpp
@@ -1,4 +1,31 @@
-int main()
+#include <iostream>
+
+enum class State
 {
-    
+    INIT,
+    SELF_TEST,
+    OPERATIONAL,
+    DEGRADED,
+    SAFE_STATE,
+    SHUTDOWN,
+};
+
+int main()
+{  
+    State current_state = State::INIT;
+    switch (current_state)
+    {
+    case State::INIT:
+        break;
+    case State::SELF_TEST:
+        break;
+    case State::OPERATIONAL:
+        break;
+    case State::DEGRADED:
+        break;
+    case State::SAFE_STATE:
+        break;
+    case State::SHUTDOWN:   
+        break;
+    }
 }
